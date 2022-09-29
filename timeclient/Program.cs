55d8554ed4350f5dc2c1519a670f39a72d0d92dd
DanilMir <mirgayazov02@gmail.com>
2022-09-29 10:28:25 +0300

    Initial commit

---
+++ timeclient/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+int port = 1303;
+
+Console.Write("Введите ip адрес сервера: ");
+var ipStr = Console.ReadLine();
+var ip = IPAddress.Parse(ipStr!);
+
+try
+{
+    IPEndPoint ipPoint = new IPEndPoint(ip, port);
+ 
+    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    // подключаемся к удаленному хосту
+    socket.Connect(ipPoint);
+ 
+    // получаем ответ
+    var data = new byte[256]; // буфер для ответа
+    StringBuilder builder = new StringBuilder();
+    int bytes = 0; // количество полученных байт
+ 
+    do
+    {
+        bytes = socket.Receive(data, data.Length, 0);
+        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+    }
+    while (socket.Available > 0);
+    Console.WriteLine(builder.ToString());
+ 
+    // закрываем сокет
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
---
+++ timeserver/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int port = 1303;
+var ipPoint = new IPEndPoint(IPAddress.Loopback, port);
+
+var listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    // связываем сокет с локальной точкой, по которой будем принимать данные
+    listenSocket.Bind(ipPoint);
+ 
+    // начинаем прослушивание
+    listenSocket.Listen(10);
+ 
+    Console.WriteLine("Сервер запущен. Ожидание подключений...");
+ 
+    while (true)
+    {
+        var handler = listenSocket.Accept();
+        
+        // Генерируем и отправляем сообщение
+        var message = DateTime.Now.ToString("dd.MM.yyyy HH:mm");
+        Console.WriteLine(message);
+        var data = Encoding.Unicode.GetBytes(message);
+        handler.Send(data);
+        
+        // закрываем сокет
+        handler.Shutdown(SocketShutdown.Both);
+        handler.Close();
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
